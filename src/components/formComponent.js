dd470b09b024263434b491efb793c927cbc57605
Ansh J Patel <patelanshj950@gmail.com>
2025-03-12 22:07:40 +0300

    updated

--- src/components/formComponent.js
+++ src/components/formComponent.js
@@ -1,22 +1,33 @@
+import React from 'react';
+import './FormComponentStyles.css';
+
 const FormComponent = () => {
-    return(
-        <div>
-            <h1>REGISTRATION</h1>
-            <label for="FirstName">First Name</label>
-            <input id="firstName" name="firstName" type="text" placeholder="First Name" required maxlength="25"/>
-            <label for="LastName">Last Name</label>
-            <input id="lastName" name="lastName" type="text" placeholder="Last Name" required maxlength="25"/>
-            <label for="Email">Email</label>
-            <input id="email" name="email" type="text" placeholder="Email" required />
-            <label for="PhoneNumber">Phone Number</label>
-            <input id="phoneNumber" name="PhoneNumber" type="number" placeholder="Phone Number" required />
-            <label for="Password"> Password</label>
-            <input id="password" name="Password" type="password" placeholder="Password" required />
-            <label for="Confirm Password">Confirm Password</label>
-            <input id="confirmPassword" name="Password" type="password" placeholder="Confirm Password" required />
-            <button id="submitButton">SUBMIT</button>
+    return (
+        <div className="form-container">
+            <h1 className="form-title">REGISTRATION</h1>
+            <form>
+                <label htmlFor="firstName">First Name</label>
+                <input id="firstName" name="firstName" type="text" className="input-field" placeholder="First Name" required maxLength="25" />
+                
+                <label htmlFor="lastName">Last Name</label>
+                <input id="lastName" name="lastName" type="text" className="input-field" placeholder="Last Name" required maxLength="25" />
+                
+                <label htmlFor="email">Email</label>
+                <input id="email" name="email" type="email" className="input-field" placeholder="Email" required />
+                
+                <label htmlFor="phoneNumber">Phone Number</label>
+                <input id="phoneNumber" name="phoneNumber" type="tel" className="input-field" placeholder="Phone Number" required />
+                
+                <label htmlFor="password">Password</label>
+                <input id="password" name="password" type="password" className="input-field" placeholder="Password" required />
+                
+                <label htmlFor="confirmPassword">Confirm Password</label>
+                <input id="confirmPassword" name="confirmPassword" type="password" className="input-field" placeholder="Confirm Password" required />
+                
+                <button type="submit" className="submit-button">SUBMIT</button>
+            </form>
         </div>
-    )
-    }
-    
-    export default FormComponent
+    );
+}
+
+export default FormComponent;
